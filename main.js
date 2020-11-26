86baecbdeb1cf49d2a76f0d1935a6e588b5acffd
Yuri Sanguino Rodriguez <=>
2020-11-25 21:09:23 -0500

    condicionales

---
+++ main.js
@@ -0,0 +1,137 @@
+//condicional
+/*let edad = prompt("Ingrese su edad");
+
+if(edad >= 18){
+    alert ("Usted es mayor de edad");
+}else{
+    alert("Usted es menor de edad");
+}*/
+
+
+//Inequilidad
+
+/*let tarjeta = true;
+
+if(tarjeta !== true){
+    alert("Entro por la puerta");
+}else{
+    alert("No puede entrar a la puerta");
+}   */
+
+//Identilidad
+//Rectifica dato y tipo de la variable
+/*let numero = true;
+let numero2 = false;
+
+if(numero === numero2){
+    alert("Entro el número")
+}else{
+    alert("El número no entro");
+}*/
+
+
+/*
+let tarjeta = true;
+
+if(tarjeta !== false){
+    alert("Entro por la puerta");
+}else{
+    alert("No puede entrar a la puerta");
+}  */
+
+/*let num = prompt("Ingrese un número mayor que (10)");
+if(num > 10){
+    alert("El número ingresado es: "+num+ " y es mayor a 10", num)
+}else{
+    alert("El número no es el correcto");
+}*/
+
+
+/*let num = prompt("Ingrese un número mayor que (10)");
+if(num >= 10){
+    alert("El número ingresado es: "+num+ " y es mayor a 10")
+}else{
+    alert("El número no es el correcto");
+}*/
+
+/*let num = prompt("Ingrese un número menor que (10)");
+if(num < 10){
+    alert("El número ingresado es: "+num+ " y es menor a 10")
+}else{
+    alert("El número no es el correcto");
+}*/
+
+/*let num = prompt("Ingrese un número menor que (10)");
+if(num <= 10){
+    alert("El número ingresado es: "+num+ " y es menor a 10")
+}else{
+    alert("El número no es el correcto");
+}*/
+
+//Operadores condición AND
+/*let a = true;
+let b = false;
+
+if(a && b){
+    alert(true);
+}else{
+    alert(false);
+}*/
+
+//Operadores condición OR
+/*let a = true;
+let b = true;
+
+if(a |= b){
+    alert(true);
+}else{
+    alert(false);
+}*/
+
+
+/*let num = 50;
+let num2 = "";
+
+
+if(num &= num2){
+    alert(true);
+}else{
+    alert(false);
+}*/
+
+
+/*let edad = prompt("Ingrese su edad");
+
+if(edad == 18){ //si
+    alert("Cumplió los 18 y es mayor de edad");
+}else if((edad >= 19) && (edad <= 20)){  //si no
+    alert("Es mayor de edad y menor a 20");
+}else if((edad >= 21) && (edad <= 25)){  //si no
+    alert("Es mayor de edad y menor a 25");
+}else{ //no
+    alert("es menor de edad")
+}*/
+
+/*let edad = prompt("Ingrese su edad");
+
+if(edad == 18){ //si
+    alert("Cumplió los 18 y es mayor de edad");
+}else if((edad >= 19) || (edad <= 20)){  //si no
+    alert("Es mayor de edad y menor a 20");
+}else if((edad >= 21) && (edad <= 25)){  //si no
+    alert("Es mayor de edad y menor a 25");
+}else{ //no
+    alert("es menor de edad")
+}*/
+
+//Funciones
+function yuri(plata){
+    let pan = 1000;
+    let salchichon = 500;
+    let vueltos;
+    
+    switch(opcion){
+    }
+    return vueltos;
+}
+alert("Mis vueltos son: "+yuri(2000));
